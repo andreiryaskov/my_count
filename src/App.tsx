0bbf5d78730bf712793c6727c5d6217f334a481a
andreiryaskov <andreiryaskov@gmail.com>
2022-02-08 20:46:34 +0300

    08_02_2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [start, setStart] = useState('')
+
+    const onChangeStartValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setStart(e.currentTarget.value)
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <input type="number"
+                       value={start}
+                       onChange={onChangeStartValue}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/input.tsx
@@ -0,0 +1,18 @@
+import React, {ChangeEvent} from 'react';
+
+type PropsType = {
+    value: string
+    onChange: (e: ChangeEvent) => void
+}
+
+
+
+const Input = (props: PropsType) => {
+    return (
+        <div>
+            i
+        </div>
+    )
+};
+
+export default Input;
